b88c590164e635c729179c437963c6ac8c7318ba
Philipp Mildenberger <philipp@mildenberger.me>
2024-04-18 23:51:27 +0200

    Fix some hashes and locks

--- pkgs/cosmic-comp/package.nix
+++ pkgs/cosmic-comp/package.nix
@@ -37,7 +37,7 @@ rustPlatform.buildRustPackage {
       "d3d12-0.19.0" = "sha256-usrxQXWLGJDjmIdw1LBXtBvX+CchZDvE8fHC0LjvhD4=";
       "glyphon-0.5.0" = "sha256-j1HrbEpUBqazWqNfJhpyjWuxYAxkvbXzRKeSouUoPWg=";
       "id_tree-1.8.0" = "sha256-uKdKHRfPGt3vagOjhnri3aYY5ar7O3rp2/ivTfM2jT0=";
-      "smithay-0.3.0" = "sha256-WllpGMhRygRc5LUvjdQFCC8ISQld8qwOjPTniSjU3uU=";
+      "smithay-0.3.0" = "sha256-daiSZCHQsOEVmysPk4Kd972Tl7ZJTNAvSWFxxqRHLmE=";
       "smithay-clipboard-0.8.0" = "sha256-OZOGbdzkgRIeDFrAENXE7g62eQTs60Je6lYVr0WudlE=";
       "smithay-egui-0.1.0" = "sha256-FcSoKCwYk3okwQURiQlDUcfk9m/Ne6pSblGAzHDaVHg=";
       "softbuffer-0.4.1" = "sha256-a0bUFz6O8CWRweNt/OxTvflnPYwO5nm6vsyc/WcXyNg=";
